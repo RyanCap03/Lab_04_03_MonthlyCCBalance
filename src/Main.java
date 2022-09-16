3a368782cd1bafc3fd114747fb4cabf2a6ae6048
RyanCap03 <capre@mail.uc.edu>
2022-09-15 22:54:31 -0400

    finished

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+        double creditBal = 5000;
+        double rate = 0.17;
+        double payment = ((creditBal *= rate));
+        double noPay = payment + payment;
+            System.out.println("Your Payment after 1 month is: $" + payment);
+            System.out.println("Your Payment after 2 months is: $" + noPay);
+
     }
 }
